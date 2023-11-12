ddbd5ae6f01babde3e117afa026bedd9d49bb90f
Frank Neblung <frank.neblung@gmail.com>
2023-11-12 14:26:07 +0100

    Below ten - green bar

--- src/main/kotlin/kata/roman/Roman.kt
+++ src/main/kotlin/kata/roman/Roman.kt
@@ -3,4 +3,15 @@ package kata.roman
 /**
  * Converts the given [nr] to its [roman representation](https://en.wikipedia.org/wiki/Roman_numerals)
  */
-fun roman(nr: Int): String = TODO()
+fun roman(nr: Int): String = when (nr) {
+    1 -> "I"
+    2 -> "II"
+    3 -> "III"
+    4 -> "IV"
+    5 -> "V"
+    6 -> "VI"
+    7 -> "VII"
+    8 -> "VIII"
+    9 -> "IX"
+    else -> error(nr)
+}
