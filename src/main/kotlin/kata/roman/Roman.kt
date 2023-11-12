1d6e7ec5ca815d67fe9248f059a6d2b06f72f399
Frank Neblung <frank.neblung@gmail.com>
2023-11-12 14:49:58 +0100

    Multiples of hundred - green bar

--- src/main/kotlin/kata/roman/Roman.kt
+++ src/main/kotlin/kata/roman/Roman.kt
@@ -4,18 +4,22 @@ package kata.roman
  * Converts the given [nr] to its [roman representation](https://en.wikipedia.org/wiki/Roman_numerals)
  */
 fun roman(nr: Int): String {
-    val factor = nr / 10
-    val rest = nr % 10
+    var factor = nr / 100
+    var rest = nr % 100
 
+    val rC = convertHundred.toRoman(factor)
+    factor = rest / 10
+    rest = rest % 10
     val rX = convertTen.toRoman(factor)
     val rI = convertOne.toRoman(rest)
-    val chars = rX + rI
+    val chars = rC + rX + rI
 
     return String(chars.toCharArray())
 }
 
 private val convertOne = DigitConverter('X', 'V', 'I')
 private val convertTen = DigitConverter('C', 'L', 'X')
+private val convertHundred = DigitConverter('M', 'D', 'C')
 
 private class DigitConverter(
     val x: Char,
