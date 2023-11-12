68a525c8bf96e9f881ba475ad648b53f2a3ed5ae
Frank Neblung <frank.neblung@gmail.com>
2023-11-12 14:28:17 +0100

    Multiples of ten - green bar

--- src/main/kotlin/kata/roman/Roman.kt
+++ src/main/kotlin/kata/roman/Roman.kt
@@ -13,5 +13,15 @@ fun roman(nr: Int): String = when (nr) {
     7 -> "VII"
     8 -> "VIII"
     9 -> "IX"
+
+    10 -> "X"
+    20 -> "XX"
+    30 -> "XXX"
+    40 -> "XL"
+    50 -> "L"
+    60 -> "LX"
+    70 -> "LXX"
+    80 -> "LXXX"
+    90 -> "XC"
     else -> error(nr)
 }
