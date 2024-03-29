ceaeb25b70e8dd77d72f1b337f1f981fc09b77d7
Frank Neblung <frank.neblung@gmail.com>
2024-03-29 11:13:50 +0100

    Refactoring

--- src/main/kotlin/kata/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/kata/fizzbuzz/FizzBuzz.kt
@@ -1,10 +1,14 @@
 package kata.fizzbuzz
 
-fun fizzbuzz(number: Int): String = when {
-    number.isRelatedTo(3) and number.isRelatedTo(5) -> "FizzBuzz"
-    number.isRelatedTo(3) -> "Fizz"
-    number.isRelatedTo(5) -> "Buzz"
-    else -> number.toString()
+fun fizzbuzz(number: Int): String {
+    val relatedTo3 = number.isRelatedTo(3)
+    val relatedTo5 = number.isRelatedTo(5)
+    return when {
+        relatedTo3 and relatedTo5 -> "FizzBuzz"
+        relatedTo3 -> "Fizz"
+        relatedTo5 -> "Buzz"
+        else -> number.toString()
+    }
 }
 
 fun Int.isRelatedTo(candidate: Int) = (this % candidate == 0) or (candidate.digitToChar() in toString())
