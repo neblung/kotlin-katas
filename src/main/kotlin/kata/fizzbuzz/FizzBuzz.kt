7629761b861eacae17e6e73ae2ecd886f63aa81f
Frank Neblung <frank.neblung@gmail.com>
2024-03-29 14:44:12 +0100

    Revert
    Revert "Refactoring"
    
    This reverts commit ceaeb25b70e8dd77d72f1b337f1f981fc09b77d7.
    
    Revert "FizzBuzz Plus. Green"
    
    This reverts commit cd3f6b52cf22ecd722307a4bf81f0bec86bf23ad.
    
    Revert "FizzBuzz Plus. Refactoring"
    
    This reverts commit 49691e878a18c2f13618c1173c866bf6c43dd4e9.
    
    --
    
    We should have come up with a better design.
    If 'business' changed mind in the past, what makes a
    number be a fizz-number, it's likely that they'll do
    in the future. So we'd better encapsulate that.

--- src/main/kotlin/kata/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/kata/fizzbuzz/FizzBuzz.kt
@@ -1,14 +1,8 @@
 package kata.fizzbuzz
 
-fun fizzbuzz(number: Int): String {
-    val relatedTo3 = number.isRelatedTo(3)
-    val relatedTo5 = number.isRelatedTo(5)
-    return when {
-        relatedTo3 and relatedTo5 -> "FizzBuzz"
-        relatedTo3 -> "Fizz"
-        relatedTo5 -> "Buzz"
-        else -> number.toString()
-    }
+fun fizzbuzz(number: Int): String = when {
+    number % 15 == 0 -> "FizzBuzz"
+    number % 3 == 0 -> "Fizz"
+    number % 5 == 0 -> "Buzz"
+    else -> number.toString()
 }
-
-fun Int.isRelatedTo(candidate: Int) = (this % candidate == 0) or (candidate.digitToChar() in toString())
