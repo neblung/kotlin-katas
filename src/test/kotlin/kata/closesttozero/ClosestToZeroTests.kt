b6f33dbb0b5bd595d019f035044a08cdbd6605f8
Frank Neblung <frank.neblung@conet-isb.de>
2024-04-15 11:36:28 +0200

    ClosestToZero Reducing to single element Red

--- src/test/kotlin/kata/closesttozero/ClosestToZeroTests.kt
+++ src/test/kotlin/kata/closesttozero/ClosestToZeroTests.kt
@@ -5,7 +5,12 @@ import org.junit.jupiter.api.Test
 
 class ClosestToZeroTests {
     @Test
-    fun `get started`() {
+    fun `single item list -- should return only element`() {
         listOf(5).closestToZero() shouldBe 5
+    }
+
+    @Test
+    fun `list of non-negatives -- should return its minimum`() {
+        listOf(10, 5, 20).closestToZero() shouldBe 5
     }
 }
