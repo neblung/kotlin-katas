c623c565cc4e4a53bea0535c44700340cb5b3b3c
Frank Neblung <frank.neblung@conet-isb.de>
2024-04-15 11:29:36 +0200

    ClosestToZero Get Started Red
    Easiest happy path I can think of

---
+++ src/main/kotlin/kata/closesttozero/README.md
@@ -0,0 +1,6 @@
+# Closest To Zero
+
+Taken from https://cyber-dojo.org/
+
+Given a list of integers find the closest to zero.
+If there is a tie, choose the positive value.
---
+++ src/test/kotlin/kata/closesttozero/ClosestToZeroTests.kt
@@ -0,0 +1,11 @@
+package kata.closesttozero
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class ClosestToZeroTests {
+    @Test
+    fun `get started`() {
+        listOf(5).closestToZero() shouldBe 5
+    }
+}
