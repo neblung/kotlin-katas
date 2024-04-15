c123c17017cff65f705648a37053781fe0e19596
Frank Neblung <frank.neblung@conet-isb.de>
2024-04-15 11:47:47 +0200

    ClosestToZero Should compare abs value Green

--- src/main/kotlin/kata/closesttozero/ClosestToZero.kt
+++ src/main/kotlin/kata/closesttozero/ClosestToZero.kt
@@ -1,9 +1,14 @@
 package kata.closesttozero
 
+import kotlin.math.abs
+
 fun List<Int>.closestToZero(): Int {
     return reduce(::closerToZero)
 }
 
 internal fun closerToZero(a: Int, b: Int): Int {
-    return minOf(a, b)
+    return when {
+        abs(a) < abs(b) -> a
+        else -> b
+    }
 }
